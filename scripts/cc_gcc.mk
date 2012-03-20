61b1fdfec23f0b2671067dac284cdecddfb5c4e8
cba <carsten.bartels@quantec-networks.de>
2012-03-20 14:22:44 +0000

    initial import
    git-svn-id: svn://rcs/development/sirius_r2/trunk@21595 71016d1f-a69f-1447-bc64-e4e72d7d84df

---
+++ scripts/cc_gcc.mk
@@ -0,0 +1,142 @@
+
+
+# Arch specific settings
+ifneq ($(ARCH),)
+ include $(ROOTDIR)/$(ARCH_PATH-y)/scripts/gcc.mk
+endif
+
+
+HOST := $(shell uname)
+export prefix
+
+# If we are using codesourcery and cygwin..
+ifneq ($(findstring CYGWIN,$(UNAME)),)
+cygpath:= $(shell cygpath -m $(shell which cygpath))
+export CYGPATH=$(cygpath)
+endif
+
+# ---------------------------------------------------------------------------
+# Compiler
+# CCFLAGS - compile flags
+
+CC	= 	$(CROSS_COMPILE)gcc
+cflags-$(CFG_OPT_RELEASE) += -O3
+cflags-$(CFG_OPT_DEBUG) += -g -O0
+
+# Remove sections if needed.. may be problems with other compilers here.
+#cflags-y += -ffunction-sections
+
+ifneq ($(filter -O2 -O3 -O1,$(cflags-y)),) 
+	cflags-y += -fno-schedule-insns -fno-schedule-insns2
+endif
+
+#cflags-y 		+= -c 
+#cflags-y 		+= -fno-common
+cflags-y 		+= -std=gnu99
+
+# Generate dependencies
+cflags-y 		+= -MMD
+
+# Warnings
+cflags-y          += -Wall
+#cflags-y          += -Winline	# warn if inline failed
+#cflags-y          += -pedantic
+
+# Conformance
+cflags-y          += -fno-strict-aliasing
+cflags-y          += -fno-builtin
+
+# Get machine cflags
+#cflags-y		+= $(cflags-$(ARCH))
+
+CFLAGS = $(cflags-y) $(cflags-yy)
+
+CCOUT 		= -o $@ 
+
+# ---------------------------------------------------------------------------
+# Preprocessor
+
+CPP	= 	$(CC) -E
+CPP_ASM_FLAGS = -x assembler-with-cpp 
+
+comma = ,
+empty = 
+space = $(empty) $(empty)
+
+# Note!
+# Libs related to GCC(libgcc.a, libgcov.a) is located under 
+# lib/gcc/<machine>/<version>/<multilib>
+# Libs related to the library (libc.a,libm.a,etc) are under:
+# <machine>/lib/<multilib>
+# 
+# Can't remember why haven't I just used gcc to link instead of ld? (it should 
+# figure out the things below by itself)
+
+# It seems some versions of make want "\=" and some "="
+# "=" - msys cpmake on windows 7 
+gcc_lib_path := "$(subst /libgcc.a,,$(shell $(CC) $(CFLAGS) --print-libgcc-file-name))" 
+gcc_lib_path := $(subst \libgcc.a,,$(gcc_lib_path)) 
+lib_lib_path := "$(subst /libc.a,,$(shell $(CC) $(CFLAGS) --print-file-name=libc.a))"
+lib_lib_path := $(subst \libc.a,,$(lib_lib_path))
+text_chunk := $(subst \,/,$(shell touch i_m_here.c; $(CC) -v -c i_m_here.c &> i_m_here.txt;gawk -f $(TOPDIR)/scripts/gcc_getinclude.awk i_m_here.txt))
+cc_inc_path := $(realpath $(text_chunk))
+libpath-y += -L$(lib_lib_path)
+libpath-y += -L$(gcc_lib_path)
+
+# ---------------------------------------------------------------------------
+# Linker
+#
+# LDFLAGS 		- linker flags
+# LDOUT   		- How to Generate linker output file
+# LDMAPFILE     - How to generate mapfile 
+# ldcmdfile-y	- link cmd file
+# libpath-y		- lib paths
+# libitem-y		- the libs with path
+# lib-y			- the libs, without path
+
+
+LD = $(CROSS_COMPILE)ld
+
+
+LD_FILE = -T
+
+LDOUT 		= -o $@
+TE = elf
+LDFLAGS += -Map $(subst .$(TE),.map, $@)
+
+libitem-y += $(libitem-yy)
+
+LD_START_GRP = --start-group
+LD_END_GRP = --end-group
+
+#LDFLAGS += --gc-section
+
+# ---------------------------------------------------------------------------
+# Assembler
+#
+# ASFLAGS 		- assembler flags
+# ASOUT 		- how to generate output file
+
+AS	= 	$(CROSS_COMPILE)as
+
+ASFLAGS += --gdwarf2
+ASOUT = -o $@
+
+# ---------------------------------------------------------------------------
+# Dumper
+
+#DDUMP          = $(Q)$(COMPILER_ROOT)/$(cross_machine-y)-objcopy
+#DDUMP_FLAGS    = -O srec
+OBJCOPY 		= $(CROSS_COMPILE)objcopy
+
+# ---------------------------------------------------------------------------
+# Archiver
+#
+# AROUT 		- archiver flags
+
+AR	= 	$(CROSS_COMPILE)ar
+AROUT 	= $@
+
+
+
+
---
+++ scripts/rules.mk
@@ -0,0 +1,283 @@
+
+
+###############################################################################
+# BUILD SETTINGS                                                              #
+###############################################################################
+
+# Board settings
+include $(ROOTDIR)/boards/$(BOARDDIR)/build_config.mk
+
+# Project settings
+-include ../build_config.mk
+
+# Perform build system version check
+include $(ROOTDIR)/scripts/version_check.mk
+
+
+
+###############################################################################
+# MODULE CONFIGURATION                                                        #
+###############################################################################
+
+# Some useful vars
+CFG_ARCH_$(ARCH):=y
+RELDIR := $(subst $(TOPDIR)/,,$(CURDIR))
+target := $(subst /,_,$(SUBDIR))
+goal=$@
+
+define MOD_AVAIL_template
+	MOD_$(1)=y
+endef
+
+define MOD_USE_template
+	USE_$(1)=y
+	def-y += USE_$(1)
+endef
+
+define CFG_template
+	CFG_$(1)=y
+	def-y += CFG_$(1)
+endef
+
+$(foreach mod,$(MOD_AVAIL),$(eval $(call MOD_AVAIL_template,${mod})))
+$(foreach mod,$(MOD_USE),$(eval $(call MOD_USE_template,${mod})))
+$(foreach mod,$(CFG),$(eval $(call CFG_template,${mod})))
+#def-y += $(ARCH) $(ARCH_FAM) $(ARCH_MCU) 
+
+# Select console / debug
+$(foreach mod,$(SELECT_OS_CONSOLE),$(eval $(call MOD_USE_template,${mod})))
+$(foreach mod,$(SELECT_CONSOLE),$(eval $(call MOD_USE_template,${mod})))
+
+def-y += SELECT_OS_CONSOLE=$(if $(SELECT_OS_CONSOLE),$(SELECT_OS_CONSOLE),TTY_NONE)
+def-y += SELECT_CONSOLE=$(if $(SELECT_CONSOLE),$(SELECT_CONSOLE),TTY_NONE)
+def-$(USE_DEBUG_PRINTF) += USE_DEBUG_PRINTF 
+
+not_avail = $(filter-out $(MOD_AVAIL),$(MOD_USE))
+ifneq ($(not_avail),)
+$(error Trying to build a module that is not available: $(not_avail))
+endif
+
+
+
+###############################################################################
+# TOOLS CONFIGURATION                                                         #
+###############################################################################
+
+# set debug optimization level as default
+ifeq ($(SELECT_OPT),)
+SELECT_OPT=OPT_DEBUG
+endif
+
+$(eval CFG_$(SELECT_OPT)=y)
+
+ARCH_PATH-y = arch/$(ARCH_FAM)/$(ARCH)
+
+# Include compiler settings
+COMPILER?=gcc
+include $(ROOTDIR)/scripts/cc_$(COMPILER).mk
+
+# Include pclint or splint settings
+ifneq ($(PCLINT),)
+include $(ROOTDIR)/scripts/cc_pclint.mk
+endif
+ifneq ($(SPLINT),)
+include $(ROOTDIR)/scripts/cc_splint.mk
+endif
+
+
+
+###############################################################################
+# PROJECT MAKEFILE                                                            #
+###############################################################################
+
+# Include project makefile
+include ../makefile
+
+# All module object files (guarded for backwards compatability)
+ifndef _BOARD_COMMON_MK
+include $(ROOTDIR)/boards/board_common.mk
+endif
+
+
+##### For backwards compatability with older project makefiles:
+
+# Remove dependency on libkernel
+deprecated-libs += $(ROOTDIR)/libs/libkernel_$(ARCH_MCU).a
+deprecated-libs-included = $(filter $(deprecated-libs),$(libitem-y))
+ifneq ($(deprecated-libs-included),)
+$(info >>>> Ignoring deprecated lib dependencies: $(deprecated-libs-included)')
+libitem-y := $(filter-out $(deprecated-libs),$(libitem-y))
+endif
+
+# Automatic preprocessing of std linkscripts
+old-ldcmdfile = $(ROOTDIR)/$(ARCH_PATH-y)/scripts/linkscript_gcc.ldf
+new-ldcmdfile = linkscript_gcc.ldp
+old-ldcmdfile-used = $(filter $(old-ldcmdfile),$(ldcmdfile-y))
+ifneq ($(old-ldcmdfile-used),)
+$(info >>>> Changing linkscript to preprocessed version: $(old-ldcmdfile) -> $(new-ldcmdfile)')
+ldcmdfile-y := $(subst $(old-ldcmdfile),$(new-ldcmdfile),$(ldcmdfile-y))
+vpath %.ldf $(ROOTDIR)/$(ARCH_PATH-y)/scripts
+endif
+
+#####
+
+inc-y += ../include
+inc-y += $(ROOTDIR)/include
+inc-y += $(ROOTDIR)/include/$(ARCH_FAM)
+inc-y += $(ROOTDIR)/include/$(ARCH_FAM)/$(ARCH)
+  
+VPATH += $(ROOTDIR)/$(SUBDIR)/src
+VPATH += $(ROOTDIR)/$(SUBDIR)
+
+
+
+###############################################################################
+# LINT                                                                        #
+###############################################################################
+
+LINT_EXCLUDE_PATHS := $(abspath $(LINT_EXCLUDE_PATHS))
+$(info $(LINT_EXCLUDE_PATHS))
+
+LINT_BAD_EXCLUDE_PATHS := $(filter %/,$(LINT_EXCLUDE_PATHS))
+ifneq ($(LINT_BAD_EXCLUDE_PATHS),)
+$(warning LINT_EXCLUDE_PATHS entries must not end in '/'. Ignoring $(LINT_BAD_EXCLUDE_PATHS))
+endif
+
+LINT_NICE_EXCLUDE_PATHS := $(filter-out %/,$(LINT_EXCLUDE_PATHS))
+LINT_NICE_EXCLUDE_PATHS := $(foreach path,$(LINT_NICE_EXCLUDE_PATHS),$(path)/)
+
+ifneq ($(PCLINT),)
+define run_pclint
+$(if 
+$(filter $(dir $(abspath $<)),$(LINT_NICE_EXCLUDE_PATHS)),
+$(info $(abspath $<):0:0: Info: Not running lint check on $(abspath $<)),
+$(Q)$(PCLINT) $(lint_extra) $(addprefix $(lintinc_ext),$(inc-y)) $(addprefix $(lintdef_ext),$(def-y)) $(abspath $<))
+endef
+endif
+
+ifneq ($(SPLINT),)
+define run_splint
+$(if 
+$(filter $(dir $(abspath $<)),$(LINT_NICE_EXCLUDE_PATHS)),
+$(info $(abspath $<):0:0: Info: Not running lint check on $(abspath $<)),
+$(Q)$(SPLINT) $(splint_extra) $(addprefix $(lintinc_ext),$(inc-y)) $(addprefix $(lintdef_ext),$(def-y)) $(abspath $<))
+endef
+endif
+
+
+
+###############################################################################
+# TOP-LEVEL TARGETS                                                           #
+###############################################################################
+
+.PHONY clean: 
+clean: FORCE
+	@-rm -f *.o *.d *.h *.elf *.a *.ldp
+
+.PHONY config: 
+config: FORCE
+	@echo "board   modules:" $(MOD_AVAIL)
+	@echo "example modules:" $(MOD_USE)
+	@echo $(MOD) ${def-y}
+
+FORCE:
+
+$(ROOTDIR)/binaries:
+	@mkdir -p $@
+
+.PHONY all:
+all: $(build-exe-y) $(build-hex-y) $(build-lib-y) $(build-bin-y) $(ROOTDIR)/binaries
+	@cp -v $(build-lib-y) $(build-exe-y) $(build-hex-y) $(build-bin-y) $(ROOTDIR)/binaries
+
+
+.SUFFIXES:
+
+
+
+###############################################################################
+# TARGETS                                                                     #
+###############################################################################
+	
+# Simple depencendy stuff
+-include $(subst .o,.d,$(obj-y))
+# Some dependency for xxx_offset.c/h also
+-include $(subst .h,.d,$(dep-y))
+
+# Compile
+%.o: %.c
+	@echo "  >> CC $(notdir $<)"
+	$(Q)$(CC) -c $(CFLAGS) -o $(goal) $(addprefix -I,$(inc-y)) $(addprefix -D,$(def-y)) $(abspath $<)
+# run lint if enabled
+	$(run_pclint)
+	$(run_splint)
+
+# Assembler
+%.o: %.s
+	@echo "  >> AS $(notdir $<)  $(ASFLAGS)"
+	$(Q)$(AS) $(ASFLAGS) -o $(goal) $<
+	
+# PP Assembler	
+#.SECONDARY %.s:
+
+%.s: %.sx
+	@echo "  >> CPP $(notdir $<)"
+	$(Q)$(CPP) $(CPP_ASM_FLAGS) -o $@ $(addprefix -I,$(inc-y)) $(addprefix -D,$(def-y)) $<
+
+# Board linker files are in the board directory 
+inc-y += $(ROOTDIR)/boards/$(BOARDDIR)
+
+# Preprocess linker files..
+%.ldp %.lcf: %.ldf
+	@echo "  >> CPP $<"
+	$(Q)$(CPP) -E -P $(CPP_ASM_FLAGS) -o $@ $(addprefix -I,$(inc-y)) $(addprefix -D,$(def-y)) $<
+
+.PHONY $(ROOTDIR)/libs:
+$(ROOTDIR)/libs:
+	$(Q)mkdir -p $@
+
+dep-y += $(ROOTDIR)/libs
+	
+# lib output
+$(build-lib-y): $(dep-y) $(obj-y)
+	@echo "  >> AR $@"   
+	$(Q)$(AR) -r -o $@ $(obj-y) 2> /dev/null
+
+# hex output
+$(build-hex-y): $(build-exe-y)
+	@echo "  >> OBJCOPY $@"   
+	$(Q)$(CROSS_COMPILE)objcopy -O ihex $< $@
+	
+# bin output
+$(build-bin-y): $(build-exe-y)
+	@echo "  >> OBJCOPY $@"   
+	$(Q)$(CROSS_COMPILE)objcopy -O binary $< $@	
+
+# Linker
+$(build-exe-y): $(dep-y) $(obj-y) $(sim-y) $(libitem-y) $(ldcmdfile-y)
+	@echo "  >> LD $@"
+ifeq ($(CROSS_COMPILE),)
+	$(Q)$(CC) $(LDFLAGS) -o $@ $(libpath-y) $(obj-y) $(lib-y) $(libitem-y)	
+else
+	$(Q)$(LD) $(LDFLAGS) $(LD_FILE) $(ldcmdfile-y) -o $@ $(libpath-y) $(LD_START_GRP) $(obj-y) $(lib-y) $(libitem-y) $(LD_END_GRP) $(LDMAPFILE)
+ ifdef CFG_MC912DG128A
+    # Print memory layout
+	@$(CROSS_COMPILE)objdump -h $@ | gawk -f $(ROOTDIR)/scripts/hc1x_memory.awk
+ else
+  ifeq ($(COMPILER),gcc)	
+    # Print memory layout
+	@echo ""
+	@gawk --non-decimal-data -f $(ROOTDIR)/scripts/gcc_map_memory.awk $(subst .elf,.map,$@)
+  endif # ($(COMPILER),gcc)
+   
+  ifeq ($(BUILD_LOAD_MODULE),y)
+	@$(CROSS_COMPILE)objcopy -O srec $@ $@.raw.s19
+	srec_cat $@.raw.s19 --crop 0x8008000 0x803fffc --fill 0x00 0x8008000 0x803fffc --l-e-crc32 0x803fffc -o $@.lm.s19
+  endif #($(BUILD_LOAD_MODULE),y)
+   
+ endif #CFG_MC912DG128A
+
+endif #($(CROSS_COMPILE),)
+	@echo
+	@echo "  >>>>>>>  DONE  <<<<<<<<<"
+	@echo
+	
