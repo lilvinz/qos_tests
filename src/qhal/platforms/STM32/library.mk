301d28a52425fe503924a168afa992282a632e75
vke <vincent.kessler@quantec-networks.de>
2013-12-24 12:43:08 +0100

    chibiostest: moved bl to fw

--- src/qhal/platforms/STM32/library.mk
+++ src/qhal/platforms/STM32/library.mk
@@ -4,6 +4,9 @@ QHAL_STM32_DIR := $(dir $(lastword $(MAKEFILE_LIST)))
 CSRC += $(wildcard $(QHAL_STM32_DIR)/*.c)
 EXTRAINCDIRS += $(QHAL_STM32_DIR)
 
+# tell ChibiOS that we are using thumb mode as STM32 does not support arm mode at all
+CFLAGS += -DTHUMB
+
 # ChibiOS
 include $(ROOT_DIR)/src/lib/ChibiOS/os/ports/GCC/ARMCMx/STM32F4xx/port.mk
 
