de931b13fb325328a184406b4d9afbb3b1147a91
vke <vincent.kessler@quantec-networks.de>
2014-01-12 23:13:46 +0100

    all chibios targets: use common/fw

--- src/common/common/library.mk
+++ src/common/common/library.mk
@@ -4,3 +4,4 @@ COMMON_COMMON_DIR := $(dir $(lastword $(MAKEFILE_LIST)))
 CSRC += $(wildcard $(COMMON_COMMON_DIR)/*.c)
 EXTRAINCDIRS += $(COMMON_COMMON_DIR)
 
+include $(COMMON_COMMON_DIR)/../library.mk
