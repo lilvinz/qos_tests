b609b46e9bbeba13b869babc211a36d734917bd9
vke <vincent.kessler@quantec-networks.de>
2014-01-30 16:22:41 +0100

    common/common: dropped

--- src/common/common/library.mk
+++
@@ -1,7 +0,0 @@
-
-
-COMMON_COMMON_DIR := $(dir $(lastword $(MAKEFILE_LIST)))
-CSRC += $(wildcard $(COMMON_COMMON_DIR)/*.c)
-EXTRAINCDIRS += $(COMMON_COMMON_DIR)
-
-include $(COMMON_COMMON_DIR)/../library.mk
