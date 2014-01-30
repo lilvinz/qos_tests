18ae0127f6f56561006f58c04cf4aa39fedee281
vke <vincent.kessler@quantec-networks.de>
2014-01-30 17:55:41 +0100

    ut_format: switch back from chprintf to format

--- src/tests/format/unittest.cpp
+++ src/tests/format/unittest.cpp
@@ -2,27 +2,27 @@
 
 extern "C"
 {
-#include "chprintf.h"
+#include "printf.h"
 }
 
 TEST(format, float)
 {
     char test[100];
 
-    EXPECT_GE(chsnprintf(test, sizeof(test), "%f", 0.999f), 0);
+    EXPECT_GE(snprintf(test, sizeof(test), "%f", 0.999f), 0);
     EXPECT_STREQ("0.999000", test);
 
-    EXPECT_GE(chsnprintf(test, sizeof(test), "%.4f", 0.999f), 0);
+    EXPECT_GE(snprintf(test, sizeof(test), "%.4f", 0.999f), 0);
     EXPECT_STREQ("0.9990", test);
 
-    EXPECT_GE(chsnprintf(test, sizeof(test), "%.3f", 0.999f), 0);
+    EXPECT_GE(snprintf(test, sizeof(test), "%.3f", 0.999f), 0);
     EXPECT_STREQ("0.999", test);
 
     // This is a rounding test.
     // Format lib has been patched not to do rounding as it fails on it.
     // Standard compliant behavior requires rounding.
     // We will fix this as a patch is available upstream.
-    EXPECT_GE(chsnprintf(test, sizeof(test), "%.2f", 0.999f), 0);
+    EXPECT_GE(snprintf(test, sizeof(test), "%.2f", 0.999f), 0);
     EXPECT_STREQ("0.99", test);
 }
 
