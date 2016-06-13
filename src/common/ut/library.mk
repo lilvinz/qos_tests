12dc674331841899147fa39ccdcab93ecee81218
vke <vincent.kessler@quantec-networks.de>
2016-06-13 20:58:09 +0200

    src/common/ut: normalize with odcp project

--- src/common/ut/library.mk
+++ src/common/ut/library.mk
@@ -4,4 +4,4 @@ COMMON_UT_DIR := $(dir $(lastword $(MAKEFILE_LIST)))
 CSRC += $(wildcard $(COMMON_UT_DIR)/*.c)
 EXTRAINCDIRS += $(COMMON_UT_DIR)
 
-include $(COMMON_UT_DIR)/../library.mk
+EXTRAINCDIRS += $(COMMON_UT_DIR)/..
